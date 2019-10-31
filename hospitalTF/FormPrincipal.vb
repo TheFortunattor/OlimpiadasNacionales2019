9117f7b2d2eabc7aaa48a081a7a31004d8464fbd
Franco Fortunatto <franfortunatto0@gmail.com>
2019-10-31 15:44:20 -0300

    Primer commit

---
+++ hospitalTF/FormPrincipal.vb
@@ -0,0 +1,28 @@
+﻿Public Class FormPrincipal
+    Private Sub FormPrincipal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            Call coneccion()
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub BtnAlarm_Click(sender As Object, e As EventArgs) Handles btnAlarm.Click
+        Me.Hide()
+        FormActivarAlarma.Show()
+
+    End Sub
+
+    Private Sub BtnViewAlarm_Click(sender As Object, e As EventArgs) Handles btnViewAlarm.Click
+        Me.Hide()
+        FormVerAlarmas.Show()
+
+
+
+    End Sub
+
+    Private Sub BtnSetRespusta_Click(sender As Object, e As EventArgs) Handles btnSetRespusta.Click
+        FormResponderLLamado.Show()
+        Me.Hide()
+    End Sub
+End Class
---
+++ hospitalTF/FormResponderLLamado.vb
@@ -0,0 +1,52 @@
+﻿Public Class FormResponderLLamado
+    Private Sub FormResponderLLamado_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ComboBoxRespuesta.Items.Clear()
+        flag = 1
+        sql = ("select fkidsec from alarma where tiempo_respuesta is NULL")
+
+        Try
+            Call ejecutar(sql)
+            While rs1.Read
+                ComboBoxRespuesta.Items.Add(rs1(0))
+            End While
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+    Private Sub BtnGoBack_Click(sender As Object, e As EventArgs) Handles btnGoBack.Click
+        Me.Close()
+        FormPrincipal.Show()
+
+    End Sub
+
+    Private Sub BtnSetRsp_Click(sender As Object, e As EventArgs) Handles btnSetRsp.Click
+        flag = 1
+        sql = ("update alarma set tiempo_respuesta = curtime() where fkidsec = '" & ComboBoxRespuesta.Text & "'")
+
+        Try
+            Call ejecutar(sql)
+            MsgBox("Alerta respondida!")
+
+
+
+
+            ComboBoxRespuesta.Items.Clear()
+            flag = 1
+            sql = ("select fkidsec from alarma where tiempo_respuesta is NULL")
+
+            Try
+                Call ejecutar(sql)
+                While rs1.Read
+                    ComboBoxRespuesta.Items.Add(rs1(0))
+                End While
+            Catch ex As Exception
+
+            End Try
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
